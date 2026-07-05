ca0f233b90d50f64760695d5ee480728af2e268e
Tbenchaphaibun <tbenchaphaibun@students.mags.school.nz>
2026-07-05 15:27:22 +1200

    tile map: water and glass

--- 2026---project/Scripts/player.gd
+++ 2026---project/Scripts/player.gd
@@ -1,24 +1,23 @@
 extends CharacterBody2D
 
-var target = position
 
-@export var speed = 200
 
+@export var speed = 100
+@export var pivot = Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+
 	
-func _input(event):
-	if event.is_action_presed(&"Click"):
-		target = get_global_mouse_position()
-		
-func _physics_process(delta: float) -> void:
-	velocity = position.direction_to(target) * speed
-	if position.distance_to(target) >2:
-		move_and_slide()
+func _process(delta: float) -> void:
+	var direction: Vector2 = Vector2(0.0,0.0)
+	direction.x = Input.get_axis("ui_left","ui_right")
+	direction.y = Input.get_axis("ui_up","ui_down")
+	velocity = speed * direction.normalized()
+	
+	move_and_slide()
 	
